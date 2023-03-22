f1a928fb61908a42aab2adbe9d235c79e98cbabc
“minJonRing” <1014080407tqr@gmail.com>
2023-03-22 15:24:54 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="background">
+    <div class="nav">
+      <router-link to="/">home</router-link>
+      <router-link to="/about">about</router-link>
+    </div>
+    <transition name="slide-fade">
+      <router-view v-slot="{ Component, route }">
+        <keep-alive>
+          <component :is="Component" :key="route.name || undefined" />
+        </keep-alive>
+      </router-view>
+    </transition>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.background {
+  height: 100%;
+  background-image: url("assets/bg.jpeg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  .nav {
+    position: fixed;
+    top: 0;
+    right: 0;
+    a {
+      display: inline-block;
+      margin: 0 6px;
+    }
+  }
+}
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: matrix(1, 0, 0, 1, 20, 0);
+  opacity: 0;
+}
+</style>
---
+++ src/components/about/index.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<template>
+  <div class="about">
+    <div>about</div>
+    <input />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.about {
+  height: 100%;
+  // background-color: #fff;
+}
+</style>
